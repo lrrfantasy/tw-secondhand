4c68ff20f9b7c67daabb0c3765cb52030f0d5dce
Guiling <monica_n88@163.com>
2017-08-03 11:22:28 +0800

    [4][Guiling] add login page.

---
+++ src/containers/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Container from '../../Container/Container';
+import Button from '../../../components/atoms/Button/Button';
+import Logo from '../../../components/Logo/Logo';
+import InputField from '../../../components/InputField/InputField';
+import VerticalSpacing from '../../../components/atoms/VerticalSpacing/VerticalSpacing';
+import './LoginPage.css';
+
+class LoginPage extends React.Component<{}, any> {
+  constructor() {
+    super();
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onLogin() {
+
+  }
+  onRegister() {
+
+  }
+
+  render() {
+    const titleProps = {
+      title: '请登录',
+      hasCloseButton: true,
+    };
+
+    return(
+      <div className="loginPage">
+        <Container titleProps={titleProps}>
+          <Logo />
+          <VerticalSpacing spacingHeightInEm={2} />
+          <InputField placeholder="用户名"/>
+          <InputField placeholder="密码" isPasswordField/>
+          <VerticalSpacing spacingHeightInEm={2} />
+          <div className="menu"><Button onClick={this.onLogin} name="登录" /></div>
+          <div className="menu"><Button onClick={this.onRegister} name="免费注册" /></div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default LoginPage
